0892f4fdd07bd3291d1ac9dd667f0a586ca56a21
Lufuno Nemadzivhanani <223247626@edu.vut.ac.za>
2025-09-01 19:37:34 +0200

    Refactor mobile menu toggle functionality

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// Toggle Menu
-function toggleMenu() {
-  document.getElementById("mobileMenu").classList.toggle("show");
-}
+// Toggle mobile menu
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+
+hamburger.addEventListener("click", () => {
+  if (mobileMenu.style.display === "flex") {
+    mobileMenu.style.display = "none";
+  } else {
+    mobileMenu.style.display = "flex";
+  }
+});
 
 // Change header on scroll
 window.addEventListener("scroll", () => {
