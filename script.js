e66741a2266e009dc6b2800bc17f6f0a737ce80a
Lufuno Nemadzivhanani <223247626@edu.vut.ac.za>
2025-09-03 14:18:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-// Toggle mobile menu
-const hamburger = document.getElementById("hamburger");
-const mobileMenu = document.getElementById("mobileMenu");
-
-hamburger.addEventListener("click", () => {
-  if (mobileMenu.style.display === "flex") {
-    mobileMenu.style.display = "none";
-  } else {
-    mobileMenu.style.display = "flex";
-  }
-});
-
-// Change header on scroll
-window.addEventListener("scroll", () => {
-  const header = document.querySelector(".header");
-  if (window.scrollY > 50) {
-    header.classList.add("scrolled");
-  } else {
-    header.classList.remove("scrolled");
-  }
-});
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.style.display = (menu.style.display === "flex") ? "none" : "flex";
+}
